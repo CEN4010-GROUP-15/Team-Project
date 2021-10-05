3042e485025934ff52e2d7a1ded24a2412afecdc
Gabriel Prieto <gabo.prieto@yahoo.com>
2021-10-05 00:11:13 -0400

    added example book endpoint

---
+++ app/book.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('../db-connection');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    try {
+      mysql.query('SELECT * FROM book', (error, results) => {
+        res.json(results);
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.get('/details/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+      mysql.query(`SELECT name, description, ISBN FROM book where book_id = ${id}`, (error, results) => {
+        res.json(results);
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+module.exports = router;
