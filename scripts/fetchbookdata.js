05c75f7b0cf4973798f86e6a468ed32aaac47fb1
Rachel Quijano <rquij005@fiu.edu>
2021-10-16 17:48:32 -0400

    Implemented details page

--- scripts/fetchbookdata.js
+++ scripts/fetchbookdata.js
@@ -6,6 +6,9 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
+function goDetails(evt){
+  location.href='./book_details.html?ISBN='+evt.currentTarget.book.ISBN;
+}
 
 const url = 'https://cen4010-group15.herokuapp.com/api/books/';
 
@@ -19,14 +22,19 @@ fetch(url)
     var objTo = document.getElementById("grid");
     console.log(objTo);
     var divtest = document.createElement("div");
+    var button = document.createElement("button");
+    button.className = "btnDetails"
+    button.innerHTML = 'Details'   
     divtest.className = "col"
     divtest.innerHTML = `
       <h3>${book.name}</h3>
       <p>by ${book.author}</p>
       <br/>
       <h4>$${book.price}</h4>
-      <img src="${book.cover_url}" alt="${book.name} cover page" />
-      <button class="btnDetails" type="button">Details</button>`;
+      <img src="${book.cover_url}" alt="${book.name} cover page" />`; 
+      button.addEventListener("click", goDetails, false);
+      button.book = book;
+      divtest.appendChild(button);
     objTo.appendChild(divtest);
   })
 })
