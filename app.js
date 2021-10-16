24ec4e95e729add585775bef695a43b0a8d13454
bennyram123 <brami057@fiu.edu>
2021-10-16 18:03:32 -0400

    added endpoint for register

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.get('/login',function(req,res) {
 app.get('/profile',function(req,res) {
   res.sendFile(__dirname + '/pages/profile.html');
 });
+app.get('/register',function(req,res) {
+  res.sendFile(__dirname + '/pages/register.html');
+});
 
 
 app.use('/api/', api);
