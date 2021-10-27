c227db8d7e4f4072442ee5f968db3f9c4116ac1e
Rachel Quijano <rquij005@fiu.edu>
2021-10-26 20:59:50 -0400

    User can add or delete book from shopping cart

--- app/shopping_cart.js
+++ app/shopping_cart.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const mysql = require('../db-connection');
 
@@ -48,5 +49,66 @@ router.get('/books', (req, res, next) => {
       next(error);
     } 
   });
+
+//Add book to shopping cart
+router.put('/addbook', (req, res, next) => {
+    const user = req.body.user;
+    const bookid = req.body.bookid;
+    try {
+        mysql.query(`SELECT books FROM shopping_cart where user_id = ${user}`, (error, results) => {
+          let books = results[0].books;
+          try {
+            if(books == ''){
+                books += `${bookid}`
+            }else{
+                books += `,${bookid}`
+            }
+              mysql.query(`UPDATE shopping_cart SET books = '${books}' WHERE  user_id = '${user}';`, (error2, results) => {
+                  console.log(results);
+                res.json(results);
+              });
+            } catch (error2) {
+              next(error2);
+            }
+        });
+      } catch (error) {
+        next(error);
+      } 
+    });
+
+
+
+//Delete book from shopping cart
+router.patch('/removebook', (req, res, next) => {
+    const user = req.body.user;
+    const bookid = req.body.bookid;
+    try {
+        mysql.query(`SELECT books FROM shopping_cart where user_id = ${user}`, (error, results) => {
+          let books = results[0].books.split(',');
+          
+          try {
+            let newBooks = '';
+            for(let i = 0; i < books.length; i++){
+                if(books[i] != bookid){
+                    if(newBooks == ''){
+                        newBooks += `${books[i]}`
+                    }else{
+                        newBooks += `,${books[i]}`
+                    }
+                }
+            }
+              mysql.query(`UPDATE shopping_cart SET books = '${newBooks}' WHERE  user_id = '${user}';`, (error2, results) => {
+                  console.log(results);
+                res.json(results);
+              });
+            } catch (error2) {
+              next(error2);
+            }
+        });
+      } catch (error) {
+        next(error);
+      } 
+    });
+
 
 module.exports = router;
